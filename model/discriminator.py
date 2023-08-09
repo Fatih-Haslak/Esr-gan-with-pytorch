871a22db6e2c65b6589070b977bc681c86fce0f1
Fatih Haslak <haslak749@gmail.com>
2023-08-09 20:45:57 +0300

    relativistic disc eklendi

--- model/discriminator.py
+++ model/discriminator.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class Discriminator(nn.Module):
+class RelativisticDiscriminator(nn.Module):
     def __init__(self, input_channels=3, hidden_channels=64):
-        super(Discriminator, self).__init__()
-        self.model = nn.Sequential(
+        super(RelativisticDiscriminator, self).__init__()
+        self.shared_layers = nn.Sequential(
             nn.Conv2d(input_channels, hidden_channels, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Conv2d(hidden_channels, hidden_channels*2, kernel_size=4, stride=2, padding=1),
@@ -16,11 +17,14 @@ class Discriminator(nn.Module):
             nn.Conv2d(hidden_channels*4, hidden_channels*8, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(hidden_channels*8),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(hidden_channels*8, 1, kernel_size=4, stride=1, padding=0),
-            #nn.Sigmoid()
-            #without sigmoid
-            #linear ag ekle
         )
+        self.final_layer = nn.Conv2d(hidden_channels*8, 1, kernel_size=4, stride=1, padding=0)
     
-    def forward(self, x):
-        return self.model(x)
+    def forward(self, real_input, fake_input):
+        real_features = self.shared_layers(real_input)
+        fake_features = self.shared_layers(fake_input)
+        
+        real_scores = self.final_layer(real_features)
+        fake_scores = self.final_layer(fake_features)
+        
+        return real_scores, fake_scores
