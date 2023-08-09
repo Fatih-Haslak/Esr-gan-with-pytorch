170fc295a5c8f64914282e91430692bf52b1e896
Fatih Haslak <haslak749@gmail.com>
2023-08-09 20:18:37 +0300

    linear ag eklencek

--- model/discriminator.py
+++ model/discriminator.py
@@ -19,6 +19,7 @@ class Discriminator(nn.Module):
             nn.Conv2d(hidden_channels*8, 1, kernel_size=4, stride=1, padding=0),
             #nn.Sigmoid()
             #without sigmoid
+            #linear ag ekle
         )
     
     def forward(self, x):
